31b4e4db591de3a8c32cee5d3098725a8926c6c5
VAC2UM <nikita.golowanev@gmail.com>
2025-10-03 22:40:47 +0300

    Реализован экран настроек

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _darkTheme = false;
+  bool _notifications = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Настройки')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            SwitchListTile(
+              title: const Text('Тёмная тема'),
+              value: _darkTheme,
+              onChanged: (value) {
+                setState(() {
+                  _darkTheme = value;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: const Text('Уведомления'),
+              value: _notifications,
+              onChanged: (value) {
+                setState(() {
+                  _notifications = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
