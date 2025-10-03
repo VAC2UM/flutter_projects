2fd7e46b0d0b968a742428bedba2239289c17d7d
VAC2UM <nikita.golowanev@gmail.com>
2025-10-03 22:25:57 +0300

    Реализован экран избранного

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({super.key});
 
   @override
+  State<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _favorites = [];
+
+  void _addFavorite() {
+    final movie = _controller.text.trim();
+    if (movie.isNotEmpty) {
+      setState(() {
+        _favorites.add(movie);
+      });
+      _controller.clear();
+    }
+  }
+
+  void _removeFavorite(int index) {
+    setState(() {
+      _favorites.removeAt(index);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Избранное')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Введите название фильма',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: _addFavorite, child: const Text('Добавить')),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _favorites.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(_favorites[index]),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () => _removeFavorite(index),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
