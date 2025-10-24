2dff1f950187fdcad8acecc009451db1d4d46bdd
VAC2UM <nikita.golowanev@gmail.com>
2025-10-24 21:56:56 +0300

    Добавлена картинка в settings_screen.dart

--- lib/features/settings/screens/settings_screen.dart
+++ lib/features/settings/screens/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -10,6 +11,8 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   bool _darkTheme = false;
   bool _notifications = true;
+  final String _imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Settings_app_icon.png';
+
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +22,22 @@ class _SettingsScreenState extends State<SettingsScreen> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              width: 200,
+              height: 200,
+              child: CachedNetworkImage(
+                imageUrl: _imageUrl,
+                progressIndicatorBuilder: (context, url, progress) =>
+                const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Center(
+                  child: Icon(
+                    Icons.error,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
             SwitchListTile(
               title: const Text('Тёмная тема'),
               value: _darkTheme,
